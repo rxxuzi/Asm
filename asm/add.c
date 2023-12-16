14ded6579d254bade4db02bdad5d90079ab79160
rxxuzi <75666622+rxxuzi@users.noreply.github.com>
2023-12-16 21:38:17 +0900

    fixed asm project

--- asm/add.c
+++ asm/add.c
@@ -3,8 +3,11 @@
 extern int add_numbers(int, int); // アセンブリ言語の関数を宣言
 
 int main() {
-    int a = 10;
-    int b = 20;
+    int a ;
+    int b ;
+    // a と b に数値を入力する
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
     int sum = add_numbers(a, b); // 関数を呼び出す
     printf("Sum: %d\n", sum);
     return 0;
